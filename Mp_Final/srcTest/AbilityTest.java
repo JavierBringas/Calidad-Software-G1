14f01868c1326ee22f9f87f5b5565526525bb723
Javier Bringas <javi.bringas.garcia@gmail.com>
2024-03-17 19:36:05 +0100

    Update AbilityTest.java

--- Mp_Final/srcTest/AbilityTest.java
+++ Mp_Final/srcTest/AbilityTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.Assertions.*;
 
 class AbilityTest {
     
